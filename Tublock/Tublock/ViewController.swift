3456955be9fafdccf5dfd95442b3f4bbe9fea23f
doyeonjeong <debby_@kakao.com>
2023-03-16 23:27:13 +0900

    🎉 [init] No Storyboard Project Setting

---
+++ Tublock/Tublock/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Tublock
+//
+//  Created by Doyeon on 2023/03/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
