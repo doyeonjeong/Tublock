73d96a2b7f357cf87cc016f1ee7cd47af8ebacb4
doyeonjeong <debby_@kakao.com>
2023-03-17 11:25:24 +0900

    🎨[style]: Set background color

--- Tublock/Tublock/Sources/Controllers/ViewController.swift
+++ Tublock/Tublock/Sources/Controllers/ViewController.swift
@@ -7,10 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        setBackground()
+    }
+    
+}
+
+extension ViewController {
+    // MARK: - Methods of set up view
+    private func setBackground() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor(red: 0.588, green: 0.39, blue: 0.592, alpha: 0.71).cgColor,
+            UIColor(red: 0.569, green: 0.632, blue: 0.962, alpha: 0.71).cgColor
+        ]
+        gradientLayer.locations = [0, 1]
+        gradientLayer.startPoint = CGPoint(x:0.0, y:0.0)
+        gradientLayer.endPoint = CGPoint(x:1.0, y:1.0)
+        gradientLayer.frame = view.bounds
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
 }
