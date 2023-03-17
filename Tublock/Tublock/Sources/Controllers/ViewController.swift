f381f5ed906d85e6d481107829da7ae32d5720df
doyeonjeong <debby_@kakao.com>
2023-03-17 11:28:15 +0900

    🎨[style]: Set Check Button

--- Tublock/Tublock/Sources/Controllers/ViewController.swift
+++ Tublock/Tublock/Sources/Controllers/ViewController.swift
@@ -12,13 +12,18 @@ final class ViewController: UIViewController {
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setBackground()
+        setup()
     }
     
 }
 
 extension ViewController {
     // MARK: - Methods of set up view
+    private func setup() {
+        setBackground()
+        setCheckButton()
+    }
+    
     private func setBackground() {
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [
@@ -30,5 +35,28 @@ extension ViewController {
         gradientLayer.endPoint = CGPoint(x:1.0, y:1.0)
         gradientLayer.frame = view.bounds
         view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    private func setCheckButton() {
+        let checkButton = UIButton(type: .system)
+        checkButton.frame = .zero
+        checkButton.translatesAutoresizingMaskIntoConstraints = false
+        checkButton.setTitle("Tap to check your message", for: .normal)
+        checkButton.setTitleColor(.white, for: .normal)
+        checkButton.backgroundColor = .black
+        checkButton.layer.cornerRadius = 10
+        checkButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        view.addSubview(checkButton)
+        
+        NSLayoutConstraint.activate([
+            checkButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            checkButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            checkButton.widthAnchor.constraint(equalToConstant: 345),
+            checkButton.heightAnchor.constraint(equalToConstant: 42)
+        ])
+    }
+    
+    @objc func buttonPressed(_ sender: UIButton) {
+        print("ButtonPressed")
     }
 }
