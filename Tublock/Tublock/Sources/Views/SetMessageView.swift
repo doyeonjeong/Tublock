2092bd7018f62ad8e705b4b1711f9dbdb805ee79
kangddong <rkdehddud96@naver.com>
2023-04-07 00:34:22 +0900

    🎨[Design]: SetMessageView 추가

---
+++ Tublock/Tublock/Sources/Views/SetMessageView.swift
@@ -0,0 +1,17 @@
+//
+//  SetMessageView.swift
+//  Tublock
+//
+//  Created by 강동영 on 2023/04/07.
+//
+
+import UIKit
+
+class SetMessageView: UIView {
+
+    private let contentsView: UIView = {
+        let view = UIView()
+        return view
+    }()
+
+}
