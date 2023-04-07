2fb636099f9c9848b1064bc0aa4338d5b0b3d0cf
doyeonjeong <debby_@kakao.com>
2023-04-07 13:31:47 +0900

    🎨[Design]: Edit UI
    - 변경된 Figma 기준에 맞게 수정

--- Tublock/Tublock/Sources/Controllers/ViewController.swift
+++ Tublock/Tublock/Sources/Controllers/ViewController.swift
@@ -18,19 +18,19 @@ final class ViewController: UIViewController {
     
     private let headerView: UIView = {
         let view = UIView()
-//        view.backgroundColor = .yellow // 범위 확인용
+        //        view.backgroundColor = .yellow // 범위 확인용
         return view
     }()
     
     private let setMaximumView: UIView = {
         let view = UIView()
-//        view.backgroundColor = .cyan // 범위 확인용
+        //        view.backgroundColor = .cyan // 범위 확인용
         return view
     }()
     
     private let setMessageView: UIView = {
         let view = UIView()
-//        view.backgroundColor = .blue // 범위 확인용
+        //        view.backgroundColor = .blue // 범위 확인용
         return view
     }()
     
@@ -52,34 +52,38 @@ extension ViewController {
     }
     
     private func addSubviews() {
-        headerView.addSubview(settingIcon)
         view.addSubview(headerView)
+        headerView.addSubview(settingIcon)
         view.addSubview(setMaximumView)
         view.addSubview(setMessageView)
     }
     
     private func setConstraints() {
         headerView.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.centerX.equalTo(view.snp.centerX)
             make.width.equalToSuperview()
-            make.height.equalTo(60)
+            make.height.equalTo(64)
         }
         
         settingIcon.snp.makeConstraints { make in
-            make.top.equalTo(headerView.snp.top).offset(24)
-            make.trailing.equalTo(headerView.snp.trailing).offset(-24)
             make.width.equalTo(24)
             make.height.equalTo(24)
+            make.top.equalTo(headerView.snp.top).offset(20)
+            make.bottom.equalTo(headerView.snp.bottom).offset(-20)
+            make.trailing.equalTo(headerView.snp.trailing).offset(-26)
         }
         
         setMaximumView.snp.makeConstraints { make in
             make.top.equalTo(headerView.snp.bottom)
+            make.centerX.equalTo(view.snp.centerX)
             make.width.equalToSuperview()
-            make.height.equalTo(180)
+            make.height.equalTo(170)
         }
         
         setMessageView.snp.makeConstraints { make in
             make.top.equalTo(setMaximumView.snp.bottom)
+            make.centerX.equalTo(view.snp.centerX)
             make.height.equalTo(250)
             make.width.equalToSuperview()
         }
