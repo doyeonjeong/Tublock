3d69699b4a5338f1d27c288d6f52b451c8cb90b7
doyeonjeong <debby_@kakao.com>
2023-04-11 15:00:25 +0900

    🐛[Fix]: DefaultSetMaximumViewModel 에 프로토콜 적용
    - BlockTime typealias를 이용하여 가독성 높임
    - 함수 내부 코드 일부때문에 작동되지 않는 오류 수정
    - 시간이 줄어듦에 따라 MaximumTime으로부터 차감하기 위해 timer 추가

---
+++ Tublock/Tublock/Sources/MainScene/Components/SetMaximum/ViewModel/DefaultSetMaximumViewModel.swift
@@ -0,0 +1,70 @@
+//
+//  SetMaximumViewModel.swift
+//  Tublock
+//
+//  Created by Doyeon on 2023/04/11.
+//
+
+import UIKit
+
+typealias BlockTime = (hours: Int, minutes: Int)
+
+protocol SetMaximumViewModel {
+    /// 선택된 시간
+    var selectedTime: BlockTime { get set }
+    
+    /// 시간이 변경될 때 호출되는 클로저
+    var onTimeChanged: (() -> Void)? { get set }
+    
+    /// `time` 속성을 지정된 값으로 업데이트
+    func update(selectedTime: BlockTime)
+
+    /// 지정된 시간만큼 시간을 줄입니다.
+    func decreaseTime(by seconds: Int)
+    
+    /// 포맷된 시간을 나타내는 `NSAttributedString`을 반환합니다.
+    func getFormattedTime() -> NSAttributedString
+    
+    /// 타이머 시작
+    func startTimer()
+    
+    /// 타이머 중지
+    func stopTimer()
+}
+
+
+class DefaultSetMaximumViewModel: SetMaximumViewModel {
+    
+    // MARK: - Properties
+    var selectedTime: BlockTime {
+        didSet {
+            onTimeChanged?()
+        }
+    }
+    
+    var onTimeChanged: (() -> Void)?
+    
+    private var _timer: Timer?
+    
+    init(selectedTime: BlockTime = (0, 0)) {
+        self.selectedTime = selectedTime
+    }
+    
+    // MARK: - Methods
+    func update(selectedTime: BlockTime) {
+    }
+    
+    func decreaseTime(by seconds: Int) {
+    }
+    
+    func getFormattedTime() -> NSAttributedString {
+        return NSAttributedString.init(string: "dummy")
+    }
+    
+    func startTimer() {
+    }
+    
+    func stopTimer() {
+    }
+
+}
