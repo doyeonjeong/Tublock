c992fdf3fa27f3aaff03f735ea06b02a64d19678
kangddong <rkdehddud96@naver.com>
2023-04-17 00:51:05 +0900

    ➕[Add]: NotificationManager class 추가

---
+++ Tublock/Tublock/Sources/Service/NotificationManager.swift
@@ -0,0 +1,19 @@
+//
+//  NotificationManager.swift
+//  Tublock
+//
+//  Created by 강동영 on 2023/04/16.
+//
+
+import Foundation
+import UserNotifications
+
+class NotificationManager {
+    
+    static let shared: NotificationManager = NotificationManager()
+    
+    private init() {}
+    
+    
+    
+}
