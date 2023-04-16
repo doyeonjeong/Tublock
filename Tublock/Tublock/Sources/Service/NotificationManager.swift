1e0e3029271b32fc2355079e007d73158d30607b
kangddong <rkdehddud96@naver.com>
2023-04-17 00:52:36 +0900

    ✨[Feat]: 새로운 기능 구현: 권한요청, 권환 확인, 스케줄 추가 기능

--- Tublock/Tublock/Sources/Service/NotificationManager.swift
+++ Tublock/Tublock/Sources/Service/NotificationManager.swift
@@ -14,6 +14,43 @@ class NotificationManager {
     
     private init() {}
     
+    private let center = UNUserNotificationCenter.current()
+        private var authorizationStatus: UNAuthorizationStatus = .authorized
+        
+        
+        func requestAuthorization() async throws -> Bool {
+            let granted = try await center.requestAuthorization(options: [.alert, .badge, .sound, .provisional])
+            
+            return granted
+        }
+        
+        func getAuthorizationStatus() -> (UNAuthorizationStatus) {
+            center.getNotificationSettings { settings in
+                self.authorizationStatus = settings.authorizationStatus
+                guard settings.authorizationStatus == .authorized || settings.authorizationStatus == .provisional else { return }
     
+            }
+            
+            return (self.authorizationStatus)
+        }
+        
+        func addScehdule() {
+            let content = UNMutableNotificationContent()
+            content.title = "Weekly Staff Meeting"
+            content.body = "Every Tuesday at 2pm"
+            
+            var dateComponents = DateComponents()
+            dateComponents.calendar = Calendar.current
+
+            dateComponents.weekday = 3  // Tuesday
+            dateComponents.hour = 14    // 14:00 hours
+               
+            // Create the trigger as a repeating event.
+            let trigger = UNCalendarNotificationTrigger(
+                     dateMatching: dateComponents, repeats: true)
+            
+            let request = UNNotificationRequest(identifier: "identifier", content: content, trigger: trigger)
+            center.add(request)
+        }
     
 }
