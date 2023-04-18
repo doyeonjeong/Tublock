d559d028fdd0fa0344cfecd4ec96290f3cd55d06
kangddong <rkdehddud96@naver.com>
2023-04-19 00:03:16 +0900

    ✨[Feat]: UserDefauls Manager 추가

---
+++ Tublock/Tublock/Sources/CommonType.swift
@@ -0,0 +1,13 @@
+//
+//  CommonType.swift
+//  Tublock
+//
+//  Created by 강동영 on 2023/04/18.
+//
+
+import Foundation
+
+final class CommonType {
+    static let MAXIMUM_TIME = "maximumTime"
+    static let NOTIFICATION_MESSEAGE = "notification contents"
+}
