5d6db86c570f14b036750cb76e7b0fd4992f6ff8
doyeonjeong <debby_@kakao.com>
2023-05-02 12:19:48 +0900

    🐛[Fix]: min -> minutes로 수정

--- Tublock/Tublock/Sources/MainScene/Components/SetMaximum/View/TimePickerModalViewController.swift
+++ Tublock/Tublock/Sources/MainScene/Components/SetMaximum/View/TimePickerModalViewController.swift
@@ -31,7 +31,7 @@ final class TimePickerModalViewController: UIViewController {
     
     private let _minutesLabel: UILabel = {
         let label = UILabel()
-        label.text = "min".localized
+        label.text = "minutes".localized
         label.textColor = .white
         return label
     }()
