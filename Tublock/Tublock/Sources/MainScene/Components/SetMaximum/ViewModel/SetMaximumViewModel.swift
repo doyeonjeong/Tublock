7aa43a8993045ca78eaf48bea59e62c716775780
doyeonjeong <debby_@kakao.com>
2023-05-08 21:23:34 +0900

    🗑️ [Del]: onTimeChanged 삭제
    - onTimeChanged 변수가 필요하지 않아 삭제하였습니다.
    - selectedTime이 변경될 때 마다 호출하기 위한 용도였으나,
      하루 1번 설정하면 변동될일이 없어 필요하지 않다고 판단했습니다.

--- Tublock/Tublock/Sources/MainScene/Components/SetMaximum/ViewModel/SetMaximumViewModel.swift
+++ Tublock/Tublock/Sources/MainScene/Components/SetMaximum/ViewModel/SetMaximumViewModel.swift
@@ -10,14 +10,7 @@ typealias BlockTime = (hours: Int, minutes: Int)
 final class SetMaximumViewModel {
     
     // MARK: - Properties
-    var selectedTime: BlockTime {
-        didSet {
-            onTimeChanged?()
-        }
-    }
-    
-    /// 시간이 변경될 때 호출되는 클로저
-    var onTimeChanged: (() -> Void)?
+    var selectedTime: BlockTime
 
     init(selectedTime: BlockTime = (0, 0)) {
         self.selectedTime = selectedTime
