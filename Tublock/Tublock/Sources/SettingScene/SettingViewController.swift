a6e6ec34be4b9413806288ceb034b8c6d039555a
kangddong <rkdehddud96@naver.com>
2023-05-10 00:35:30 +0900

    ➕[Add]: SettingViewController 추가

---
+++ Tublock/Tublock/Sources/SettingScene/SettingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingViewController.swift
+//  Tublock
+//
+//  Created by 강동영 on 2023/05/10.
+//
+
+import UIKit
+
+final class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
