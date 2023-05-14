442d65ad6a5bbcf2b1089be2eadea322646eb679
doyeonjeong <debby_@kakao.com>
2023-05-14 16:51:34 +0900

    ✨[Feat]: 앱 차단 & 차단 해제 기능

--- Tublock/Tublock/Sources/Service/BlockingManager.swift
+++ Tublock/Tublock/Sources/Service/BlockingManager.swift
@@ -29,6 +29,8 @@ final class BlockingApplicationModel: ObservableObject {
 // MARK: - Manager
 final class BlockingManager {
     
+    private let _store = ManagedSettingsStore()
+    private let _model = BlockingApplicationModel.shared
     private let _center = AuthorizationCenter.shared
     
     func requestAuthorization() {
@@ -39,5 +41,33 @@ final class BlockingManager {
                 print(error.localizedDescription)
             }
         }
+    }
+    
+    func block(completion: @escaping (Result<Void, Error>) -> Void) {
+        // 선택한 앱 토큰 가져오기
+        let selectedAppTokens = _model.selectedAppsTokens
+        
+        // DeviceActivityCenter를 사용하여 모든 선택한 앱 토큰에 대한 액티비티 차단
+        let deviceActivityCenter = DeviceActivityCenter()
+        
+        // 모니터 DeviceActivitySchedule 설정
+        let blockSchedule = DeviceActivitySchedule(
+            intervalStart: DateComponents(hour: 0, minute: 0),
+            intervalEnd: DateComponents(hour: 23, minute: 59),
+            repeats: true
+        )
+        
+        _store.shield.applications = selectedAppTokens
+        do {
+            try deviceActivityCenter.startMonitoring(DeviceActivityName.daily, during: blockSchedule)
+        } catch {
+            completion(.failure(error))
+            return
+        }
+        completion(.success(()))
+    }
+    
+    func unblockAllApps() {
+        _store.shield.applications = []
     }
 }
