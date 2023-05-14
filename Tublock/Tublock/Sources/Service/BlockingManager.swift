6c274b31db863448aa79e8c2b4b7ac1b3e5af73b
doyeonjeong <debby_@kakao.com>
2023-05-14 16:50:27 +0900

    ➕[Add]: BlockingApplicationModel 추가

--- Tublock/Tublock/Sources/Service/BlockingManager.swift
+++ Tublock/Tublock/Sources/Service/BlockingManager.swift
@@ -5,14 +5,28 @@
 //  Created by 강동영 on 2023/04/10.
 //
 
+import Foundation
 import FamilyControls
 import DeviceActivity
 import ManagedSettings
 
+// MARK: - Extension
 extension DeviceActivityName {
     static let daily = Self("daily")
 }
 
+// MARK: - Model
+final class BlockingApplicationModel: ObservableObject {
+    static let shared = BlockingApplicationModel()
+    
+    @Published var newSelection: FamilyActivitySelection = .init()
+    
+    var selectedAppsTokens: Set<ApplicationToken> {
+        newSelection.applicationTokens
+    }
+}
+
+// MARK: - Manager
 final class BlockingManager {
     
     private let _center = AuthorizationCenter.shared
